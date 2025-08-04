c55ff1ae57db1a819694d5557c973d78eacd3f75
PEDRO HENRIQUE SANTOS MEIRA <CG3036537@car.ifsp>
2025-08-04 17:54:53 -0300

    Familiarizando com React

--- App.js
+++ App.js
@@ -4,7 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box1}>
+        <View style={styles.box3}></View>
+        <View style={styles.box3}></View>
+      </View>
+        <Text style={styles.tituloPrincipal}>Olá Mundo!</Text>
+        <Text style={styles.subtitulo}>Subtitulo</Text>
+        <Text style={styles.paragrafo}>Este é um paragrafo</Text>
+        <Text style={styles.saibaMais}>Saiba Mais!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +23,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: 'arial',
   },
+
+
+  tituloPrincipal: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+
+  subtitulo: {
+    fontSize: 18,
+  },
+
+  box1: {
+    width: 400, 
+    height: 100,
+    backgroundColor: '#2e5a88',
+    justifyContent: 'space-between',
+  },
+
+  
 });
